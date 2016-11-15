0a98087ba1fe50cd3815b5f9b2542103470f7487
aleoaninda <aleoaninda@gmail.com>
2016-11-15 23:13:59 +0600

    Update register.php

--- register.php
+++ register.php
@@ -14,10 +14,7 @@ by Aleo Aninda (ID: 13301113)
     </head>
     <body>
       <?php
-        $database = new mysqli("localhost","root","ark112angel","gamebox_db"); 
-        if($database->connect_errno){
-          echo "Failed to connect to MySQL: (" . $database->connect_errno . ") ";
-        }
+        include 'database_auth.php';
 
         if(isset($_POST["registersubmit"])){
           $password=md5($_POST["password"]);
