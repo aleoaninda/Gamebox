04cc4ac1ff40683c272381735b7f83442a38218e
aleoaninda <aleoaninda@gmail.com>
2016-11-15 23:14:31 +0600

    Update removeproduct.php

--- removeproduct.php
+++ removeproduct.php
@@ -1,10 +1,7 @@
 <?php
     session_start();
     if(isset($_POST['removeitem'])){
-        $database = new mysqli("localhost","root","ark112angel","gamebox_db"); 
-        if($database->connect_errno){
-        	echo "Failed to connect to MySQL: (" . $database->connect_errno . ") " . $database->connect_error;
-        }
+        include 'database_auth.php';
         $c_id=$_SESSION["CUSTOMER_ID"];
         $p_id=$_POST["product_id"];
         $sql="DELETE FROM `cart` WHERE p_id='$p_id' AND c_id='$c_id'";
